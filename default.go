b964edb01019ac965800a50b9eeabf540dd8e604
akm <akm2000@gmail.com>
2024-10-27 18:34:20 +0900

    ♻️ Move Wrap function from default.go to wrap.go

--- default.go
+++ default.go
@@ -1,15 +1,9 @@
 package slogw
 
-import "log/slog"
-
 var defaultWrapFuncs WrapFuncs
 
 func RegisterWrapFunc(f WrapFunc) {
 	defaultWrapFuncs = append(defaultWrapFuncs, f)
-}
-
-func Wrap(h slog.Handler) slog.Handler {
-	return defaultWrapFuncs.Wrap(h)
 }
 
 func Register(f func(HandleFunc) HandleFunc) {
--- wrap.go
+++ wrap.go
@@ -2,6 +2,10 @@ package slogw
 
 import "log/slog"
 
+func Wrap(h slog.Handler) slog.Handler {
+	return defaultWrapFuncs.Wrap(h)
+}
+
 func New(h slog.Handler) *slog.Logger {
 	return slog.New(Wrap(h))
 }
