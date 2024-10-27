876bbd486a75dcae88de920adbebfb980243444f
akm <akm2000@gmail.com>
2024-10-27 18:26:49 +0900

    ♻️ Rename handleWrapper to wrapper

--- handle_wraper.go
+++ handle_wraper.go
@@ -7,20 +7,20 @@ import (
 
 type HandleFunc = func(context.Context, slog.Record) error
 
-type handleWrapper struct {
+type wrapper struct {
 	slog.Handler
 	handle HandleFunc
 }
 
-var _ slog.Handler = (*handleWrapper)(nil)
+var _ slog.Handler = (*wrapper)(nil)
 
 func NewHandleWrapFunc(fn func(orig HandleFunc) HandleFunc) WrapFunc {
 	return func(h slog.Handler) slog.Handler {
 		handle := fn(h.Handle)
-		return &handleWrapper{Handler: h, handle: handle}
+		return &wrapper{Handler: h, handle: handle}
 	}
 }
 
-func (h *handleWrapper) Handle(ctx context.Context, rec slog.Record) error {
+func (h *wrapper) Handle(ctx context.Context, rec slog.Record) error {
 	return h.handle(ctx, rec)
 }
