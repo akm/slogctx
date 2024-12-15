7fa6cbe7fed050851b5fc40d0d2eda01997eef0e
akm <akm2000@gmail.com>
2024-12-15 22:07:11 +0900

    ♻️ Move NewWrapFunc definition from wrapper.go to register.go

--- register.go
+++ register.go
@@ -1,5 +1,7 @@
 package slogw
 
+import "log/slog"
+
 var defaultWrapFuncs HandlerWrapFuncs
 
 func RegisterWrapFunc(f HandlerWrapFunc) {
@@ -8,4 +10,11 @@ func RegisterWrapFunc(f HandlerWrapFunc) {
 
 func Register(f func(HandleFunc) HandleFunc) {
 	RegisterWrapFunc(NewWrapFunc(f))
+}
+
+func NewWrapFunc(fn func(orig HandleFunc) HandleFunc) HandlerWrapFunc {
+	return func(h slog.Handler) slog.Handler {
+		handle := fn(h.Handle)
+		return &wrapper{Handler: h, handle: handle}
+	}
 }
--- wrapper.go
+++ wrapper.go
@@ -12,13 +12,6 @@ type wrapper struct {
 
 var _ slog.Handler = (*wrapper)(nil)
 
-func NewWrapFunc(fn func(orig HandleFunc) HandleFunc) HandlerWrapFunc {
-	return func(h slog.Handler) slog.Handler {
-		handle := fn(h.Handle)
-		return &wrapper{Handler: h, handle: handle}
-	}
-}
-
 func (h *wrapper) Handle(ctx context.Context, rec slog.Record) error {
 	return h.handle(ctx, rec)
 }
