293f54be1fb9b420290d2e5bc2b489028ff98805
akm <akm2000@gmail.com>
2025-01-09 21:58:26 +0900

    ✅ Add tests for Default and SetDefault function

---
+++ default_test.go
@@ -0,0 +1,20 @@
+package slogw
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefault(t *testing.T) {
+	defaultBackup := defaultNamespace
+	defer func() {
+		defaultNamespace = defaultBackup
+	}()
+
+	assert.Equal(t, defaultBackup, Default())
+
+	ns := NewNamespace()
+	SetDefault(ns)
+	assert.Equal(t, ns, Default())
+}
