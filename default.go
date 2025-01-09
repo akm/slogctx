52f718734e4e85672e5f8e9547afa0a7f06ba53f
akm <akm2000@gmail.com>
2025-01-09 22:02:05 +0900

    🔥 Remove function which is not used generally

--- default.go
+++ default.go
@@ -12,24 +12,8 @@ func SetDefault(ns *Namespace) {
 	defaultNamespace = ns
 }
 
-func RegisterHandlerWrapFunc(f HandlerWrapFunc) {
-	defaultNamespace.RegisterHandlerWrapFunc(f)
-}
-
-func RegisterHandlerPrepareFunc(f HandlePrepareFunc) {
-	defaultNamespace.RegisterHandlerPrepareFunc(f)
-}
-
-func RegisterHandleFuncWrapFunc(fn HandleFuncWrapFunc) {
-	defaultNamespace.RegisterHandleFuncWrapFunc(fn)
-}
-
 func Register(f HandlePrepareFunc) {
 	defaultNamespace.Register(f)
-}
-
-func Wrap(h slog.Handler) slog.Handler {
-	return defaultNamespace.Wrap(h)
 }
 
 func New(h slog.Handler) *slog.Logger {
