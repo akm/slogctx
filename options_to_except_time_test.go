67d8e50f4cadebce5f5e2f33853a13f422adfd8e
akm <akm2000@gmail.com>
2025-02-09 08:52:34 +0900

    ✅ Use underscore for not used argument

--- options_to_except_time_test.go
+++ options_to_except_time_test.go
@@ -3,7 +3,7 @@ package slogctx_test
 import "log/slog"
 
 var testOptions = &slog.HandlerOptions{
-	ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+	ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
 		if a.Key == slog.TimeKey {
 			return slog.Attr{}
 		}
