24fac7153629c3881f4ee00b49466f29dc27762d
akm <akm2000@gmail.com>
2025-02-08 15:25:28 +0900

    ♻️ Move HandlerConv and its related definitions from handle.go to handler_covn.go

--- handle.go
+++ handle.go
@@ -17,25 +17,3 @@ func PrepareConv(prepare RecordPrepare) HandleConv {
 		}
 	}
 }
-
-type HandlerConv = func(slog.Handler) slog.Handler
-
-func NewHandlerConv(fn HandleConv) HandlerConv {
-	return func(h slog.Handler) slog.Handler {
-		handle := fn(h.Handle)
-		return &wrapper{Handler: h, handle: handle}
-	}
-}
-
-type HandlerConvs []HandlerConv
-
-func (fns HandlerConvs) Wrap(h slog.Handler) slog.Handler {
-	for i := len(fns) - 1; i >= 0; i-- {
-		h = fns[i](h)
-	}
-	return h
-}
-
-func (fns HandlerConvs) New(h slog.Handler) *slog.Logger {
-	return slog.New(fns.Wrap(h))
-}
--- handler_conv.go
+++ handler_conv.go
@@ -15,3 +15,25 @@ var _ slog.Handler = (*wrapper)(nil)
 func (h *wrapper) Handle(ctx context.Context, rec slog.Record) error {
 	return h.handle(ctx, rec)
 }
+
+type HandlerConv = func(slog.Handler) slog.Handler
+
+func NewHandlerConv(fn HandleConv) HandlerConv {
+	return func(h slog.Handler) slog.Handler {
+		handle := fn(h.Handle)
+		return &wrapper{Handler: h, handle: handle}
+	}
+}
+
+type HandlerConvs []HandlerConv
+
+func (fns HandlerConvs) Wrap(h slog.Handler) slog.Handler {
+	for i := len(fns) - 1; i >= 0; i-- {
+		h = fns[i](h)
+	}
+	return h
+}
+
+func (fns HandlerConvs) New(h slog.Handler) *slog.Logger {
+	return slog.New(fns.Wrap(h))
+}
