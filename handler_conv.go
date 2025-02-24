5364c39449c22797213ef897a309e86be45a9292
akm <akm2000@gmail.com>
2025-02-25 01:51:03 +0900

    ✨ Support WithAttrs and WithGroup

--- handler_conv.go
+++ handler_conv.go
@@ -6,14 +6,35 @@ import (
 )
 
 type wrapper struct {
-	slog.Handler
-	handle Handle
+	impl          slog.Handler
+	handle        Handle
+	handleFactory func(slog.Handler) Handle
 }
 
 var _ slog.Handler = (*wrapper)(nil)
 
+func newWrapper(h slog.Handler, f func(slog.Handler) Handle) *wrapper {
+	return &wrapper{
+		impl:          h,
+		handleFactory: f,
+		handle:        f(h),
+	}
+}
+
+func (h *wrapper) Enabled(ctx context.Context, lv slog.Level) bool {
+	return h.impl.Enabled(ctx, lv)
+}
+
 func (h *wrapper) Handle(ctx context.Context, rec slog.Record) error {
 	return h.handle(ctx, rec)
+}
+
+func (h *wrapper) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return newWrapper(h.impl.WithAttrs(attrs), h.handleFactory)
+}
+
+func (h *wrapper) WithGroup(name string) slog.Handler {
+	return newWrapper(h.impl.WithGroup(name), h.handleFactory)
 }
 
 // HandleConv is a function that converts a Handle function.
@@ -23,6 +44,6 @@ type HandlerConv = func(slog.Handler) slog.Handler
 func NewHandlerConv(fn HandleConv) HandlerConv {
 	return func(h slog.Handler) slog.Handler {
 		handle := fn(h.Handle)
-		return &wrapper{Handler: h, handle: handle}
+		return &wrapper{impl: h, handle: handle}
 	}
 }
